229cad31567f1ea7ba055d2cd408d07f52497637
Mohammedaoudi <meddaou2003@gmail.com>
2023-10-16 08:37:48 -0700

    tasks

---
+++ README.md
@@ -0,0 +1,1 @@
+ alx task and project
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+
+
+int _printf(const char *format, ...);
+void i_to_str(char buffer[], int num);
+int _putchar(char c);
+int _strlen(char *str);
+int print(char *string);
+
+#endif
